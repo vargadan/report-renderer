1b49f57b02abb85c8dab47121b40e7248c3d3128
vargadan <danielvrg@gmail.com>
2017-09-24 12:26:12 +0200

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,7 +20,7 @@ node('maven') {
 
    
    	stage ('Static Analysis') {
- 		sh "export MAVEN_OPTS='-Xms128m -Xmx128m'; ${mvnCmd} org.jacoco:jacoco-maven-plugin:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000/ -DskipTests=true"
+ 		sh "export MAVEN_OPTS='-XX:MaxPermSize=256M  -Xms512m -Xmx512m'; ${mvnCmd} org.jacoco:jacoco-maven-plugin:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000/ -DskipTests=true"
    	}
    	
    	stage ('Test') {
