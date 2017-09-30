44aa28526a3522e6c91084e17993d40f5b18a580
vargadan <danielvrg@gmail.com>
2017-10-01 01:49:50 +0200

    OAuth2RestTemplate

--- src/main/java/net/vargadaniel/reportengine/reportrenderer/ReportRendererApp.java
+++ src/main/java/net/vargadaniel/reportengine/reportrenderer/ReportRendererApp.java
@@ -2,6 +2,7 @@ package net.vargadaniel.reportengine.reportrenderer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
@@ -20,6 +21,7 @@ public class ReportRendererApp {
 		SpringApplication.run(ReportRendererApp.class, args);
 	}
 
+	@Bean
 	public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
 			OAuth2ProtectedResourceDetails details) {
 		return new OAuth2RestTemplate(details, oauth2ClientContext);
