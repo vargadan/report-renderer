1c686cf96d1a36b03f892902cd6fc4c3292a2218
vargadan <danielvrg@gmail.com>
2017-10-01 01:22:50 +0200

    oauth

--- src/main/java/net/vargadaniel/reportengine/reportrenderer/ReportRendererApp.java
+++ src/main/java/net/vargadaniel/reportengine/reportrenderer/ReportRendererApp.java
@@ -2,7 +2,9 @@ package net.vargadaniel.reportengine.reportrenderer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class ReportRendererApp {
 	
